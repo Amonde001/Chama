753ac50a0907d439876fc42d98570f78c81f67af
Amonde001 <vinnyamonde@gmail.com>
2022-11-11 23:09:31 +0300

    initial commt'

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*Show sideBar*/
+function  ShowsideBar(){
+    var sidebar = document.getElementById("sidebar");
+    sidebar.style.display = 'block';
+}
+/*hide Sidebar*/
+function HidesideBar(){
+    var sidebar = document.getElementById('sidebar');
+    sidebar.style.display = 'none';
+}
